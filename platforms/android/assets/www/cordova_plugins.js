ec332ae0cf722a4f59824a32d559541293a2bd9b
stefano.salidu <stefano.salidu@git.flosslab.com>
2016-10-13 15:18:20 +0200

    importing

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,74 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-device.device",
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "pluginId": "cordova-plugin-splashscreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "id": "cordova-plugin-statusbar.statusbar",
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "pluginId": "cordova-plugin-statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "id": "ionic-plugin-keyboard.keyboard",
+        "file": "plugins/ionic-plugin-keyboard/www/android/keyboard.js",
+        "pluginId": "ionic-plugin-keyboard",
+        "clobbers": [
+            "cordova.plugins.Keyboard"
+        ],
+        "runs": true
+    },
+    {
+        "id": "cordova-plugin-fullscreen.AndroidFullScreen",
+        "file": "plugins/cordova-plugin-fullscreen/www/AndroidFullScreen.js",
+        "pluginId": "cordova-plugin-fullscreen",
+        "clobbers": [
+            "AndroidFullScreen"
+        ]
+    },
+    {
+        "id": "com.ezartech.ezar.videooverlay.videoOverlay",
+        "file": "plugins/com.ezartech.ezar.videooverlay/www/videoOverlay.js",
+        "pluginId": "com.ezartech.ezar.videooverlay",
+        "merges": [
+            "ezar"
+        ]
+    },
+    {
+        "id": "com.ezartech.ezar.videooverlay.camera",
+        "file": "plugins/com.ezartech.ezar.videooverlay/www/camera.js",
+        "pluginId": "com.ezartech.ezar.videooverlay",
+        "clobbers": [
+            "camera"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-device": "1.1.3",
+    "cordova-plugin-console": "1.0.4",
+    "cordova-plugin-whitelist": "1.3.0",
+    "cordova-plugin-splashscreen": "4.0.0",
+    "cordova-plugin-statusbar": "2.2.0",
+    "ionic-plugin-keyboard": "2.2.1",
+    "cordova-plugin-fullscreen": "1.1.0",
+    "com.ezartech.ezar.videooverlay": "0.2.4"
+};
+// BOTTOM OF METADATA
+});
